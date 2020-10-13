a32aa57a8cce4d40fb7bdcc825d50e5e0ca4e6fc
MANISHA KUMARI <45851853+iit2018062@users.noreply.github.com>
2020-10-14 00:24:14 +0530

    Update iit2018062_nqueen_final.cpp

--- iit2018062_nqueen_final.cpp
+++ iit2018062_nqueen_final.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int min_value(vector<vector<int> >&domain,vector<bool>&visited)
 {  
-    int  m =INT_MAX;
+   int m =INT_MAX;
    int n = domain.size();
    int c=0;
    int col;
@@ -23,8 +23,7 @@ int min_value(vector<vector<int> >&domain,vector<bool>&visited)
    return col;
 }
 int helper(int row,int col,vector<vector<int> >&domain,int n)
-{  //cout<<"inside helper\n";
-   //remove the row 
+{  
    int c=0;
    for(int i=0;i<n;i++)
    {
@@ -36,8 +35,6 @@ int helper(int row,int col,vector<vector<int> >&domain,int n)
     	if(i!=row)
     	domain[i][col]=-1;
 	}
-	   //remove theh diagonal rows;
-	   //lower diagonal
 	   for(int i=row+1,j=col+1;j<n &&i<n;j++,i++)
 	   domain[i][j]=-1;
 	   
@@ -50,8 +47,9 @@ int helper(int row,int col,vector<vector<int> >&domain,int n)
    
 }
 bool over(vector<vector<int> >&g,int n)
-{  //cout<<"lll\n";
+{ 
 	int c=0;
+	
 	for(int i=0;i<n;i++)
 	{
 		for(int j=0;j<n;j++)
@@ -85,7 +83,7 @@ bool revised(vector<vector<int> >&domain,int col,int row,int n)
 	return true;
 	
 }
-bool fun(vector<vector<int> >&g,vector<vector<int> >&d,int n,vector<bool>&visited)
+bool AC3(vector<vector<int> >&g,vector<vector<int> >&d,int n,vector<bool>&visited)
 { 
 	
 	vector<vector<int> >s = d;
@@ -93,10 +91,12 @@ bool fun(vector<vector<int> >&g,vector<vector<int> >&d,int n,vector<bool>&visite
       q.push(d);
       while(!q.empty())
       {
-      	
+      	 //cout<<"babes\n";
       	vector<vector<int> >tempo=q.front();
       	q.pop();
-      	if(over(tempo,n))return true;
+      	if(over(g,n)){//cout<<"wallah\n";
+      
+		  return true;}
       	int col = min_value(tempo,visited);
      	visited[col]=true;
        for(int i=0;i<n;i++)
@@ -106,8 +106,8 @@ bool fun(vector<vector<int> >&g,vector<vector<int> >&d,int n,vector<bool>&visite
 	if(g[i][col]==0 && tempo[i][col]!=-1 && revised(tempo,col,i,n) )
 	{
 		g[i][col]=1;
-	
-		bool flag = fun(g,tempo,n,visited);
+	    // cout<<"maniha\n";
+		bool flag = AC3(g,tempo,n,visited);
 		if(flag)return true;
 		q.push(tmp);
 		tempo= tmp;
@@ -119,6 +119,8 @@ bool fun(vector<vector<int> >&g,vector<vector<int> >&d,int n,vector<bool>&visite
       	else tempo = tmp;
 		
 	   }
+	   visited[col]=false;
+	   return false;
 
 	  }
 
@@ -129,6 +131,7 @@ bool fun(vector<vector<int> >&g,vector<vector<int> >&d,int n,vector<bool>&visite
 int main()
 {
 	int n;
+	cout<<"please enter the dimension of square chess board\n";
 	cin>>n;
     vector<vector<int> >domain(n,vector<int>(n,0));
     for(int i=0;i<n;i++)
@@ -140,7 +143,7 @@ int main()
     vector<vector<int> >chess(n,vector<int>(n,0));
     
     vector<bool>visited(n,false);
-    bool f=fun(chess,domain,n,visited);
+    bool f=AC3(chess,domain,n,visited);
     cout<<"0 means no queen in place while 1 means a queen in the cell, -1 means no solution\n";
     if(f)
     {
